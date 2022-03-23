f8b0ed063289b50b23cdfebad3b3575391af612f
Michael Chilaka <obichilaka@gmail.com>
2022-03-23 11:36:54 +0100

    setup, folder structure, updates on App.js

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    fetch()
+  }, [])
+  
+  return (
+    <>
+    Hello Working!
+    </>
+  );
+}
+
+export default App;
